773e3ac273ddb8c6ef17156dc824045dfd874592
peterku <peterschoice@gmail.com>
2022-12-05 13:12:49 +0900

    최초파일

---
+++ module/Controller/Mobile/Main/IndexController.php
@@ -0,0 +1,32 @@
+<?php
+
+/**
+ * This is commercial software, only users who have purchased a valid license
+ * and accept to the terms of the License Agreement can install and use this
+ * program.
+ *
+ * Do not edit or add to this file if you wish to upgrade Godomall5 to newer
+ * versions in the future.
+ *
+ * @copyright ⓒ 2016, NHN godo: Corp.
+ * @link http://www.godo.co.kr
+ */
+namespace Controller\Mobile;
+
+/**
+ * 사이트 접속 페이지
+ *
+ * @author Jong-tae Ahn <qnibus@godo.co.kr>
+ */
+class IndexController extends \Controller\Front\IndexController
+{
+    /**
+     * {@inheritdoc}
+     */
+    public function index()
+    {
+        //
+        parent::index();
+
+    }
+}
