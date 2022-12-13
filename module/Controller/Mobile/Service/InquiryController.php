62cd66e2370bf196fc9dbd984cff7a0c98e1f73b
peterku <peterschoice@gmail.com>
2022-12-13 14:47:03 +0900

    상속파일

---
+++ module/Controller/Mobile/Service/InquiryController.php
@@ -0,0 +1,33 @@
+<?php
+
+/**
+ * This is commercial software, only users who have purchased a valid license
+ * and accept to the terms of the License Agreement can install and use this
+ * program.
+ *
+ * Do not edit or add to this file if you wish to upgrade Godomall5 to newer
+ * versions in the future.
+ *
+ * @copyright ⓒ 2016, NHN godo: Corp.
+ * @link http://www.godo.co.kr
+ */
+namespace Controller\Mobile\Service;
+
+
+use Request;
+/**
+ * 사이트 접속 페이지
+ *
+ * @author Jong-tae Ahn <qnibus@godo.co.kr>
+ */
+class InquiryController extends \Controller\Front\Service\InquiryController
+{
+    /**
+     * {@inheritdoc}
+     */
+    public function index()
+    {
+
+        parent::index();
+    }
+}
