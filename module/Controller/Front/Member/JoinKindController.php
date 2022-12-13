7cf1fad1e4bbedca0aae053909d564c2220b3326
peterku <peterschoice@gmail.com>
2022-12-14 08:53:35 +0900

    join상속

---
+++ module/Controller/Front/Member/JoinKindController.php
@@ -0,0 +1,21 @@
+<?php
+
+namespace Controller\Front\Member;
+
+/**
+ * 회원가입
+ *
+ * @author Jong-tae Ahn <qnibus@godo.co.kr>
+ */
+class JoinKindController extends \Bundle\Controller\Front\Member\JoinController
+{
+    /**
+     * {@inheritdoc}
+     */
+    public function index()
+    {
+
+        parent::index();
+
+    }
+}
