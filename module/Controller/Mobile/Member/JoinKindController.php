d2610ae53cd3dccf646c98c5e14221b791cfb8b6
peterku <peterschoice@gmail.com>
2022-12-14 10:09:57 +0900

    최초파일

---
+++ module/Controller/Mobile/Member/JoinKindController.php
@@ -0,0 +1,21 @@
+<?php
+
+namespace Controller\Mobile\Member;
+
+/**
+ * 회원가입
+ *
+ * @author Jong-tae Ahn <qnibus@godo.co.kr>
+ */
+class JoinKindController extends \Bundle\Controller\Mobile\Member\JoinMethodController
+{
+    /**
+     * {@inheritdoc}
+     */
+    public function index()
+    {
+
+        parent::index();
+
+    }
+}
